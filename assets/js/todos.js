98c163e0afa323c3539e179c41af810d577dac04
Jamie <jamiemckeown469@gmail.com>
2018-09-26 16:52:28 -0400

    push project to github

---
+++ assets/js/todos.js
@@ -0,0 +1,39 @@
+
+// when an li is clicked inside a ul
+// run line 7
+
+
+$("ul").on("click","li", function(){
+    $(this).toggleClass("completed");
+});
+
+// click on x to delete to-do
+
+$("span").on("click", function(event){
+    // below code is to remove the parent element
+    // in this case the parent of "span" which is "li"
+    // fadeout for 5 seconds then remove
+    $(this).parent().fadeOut(1000,function(){
+        $(this).remove();
+    });
+    // below code is to prevent event "bubbling"
+    // note that to do below, "event" must be 
+    // used as argument of function
+    event.stopPropagation();
+});
+
+
+// add user input to bottom of list
+// specified we only want input of type "text"
+
+$("input[type='text']").on("keypress", function(event){
+    // to check if keypress is"enter"
+    if(event.which === 13){
+    // assign what was entered to "todoText"
+        var todoText = $(this).val();
+    //   after enter pressed clear the display
+        $(this).val("");
+    // create a new li and add it to ul
+      $("ul").append("<li><span>X</span> " + todoText + "</li>")
+    }
+});
